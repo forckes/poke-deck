4023b812c20f7af49b6d5a093a0e3ce91ec19114
forckes <forckes@proton.me>
2026-08-05 13:09:06 +0200

    added allowed hosts for auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,6 +6,12 @@ import { dash } from '@better-auth/infra'
 import { isE2E } from '@/utils/isE2E'
 console.log('auth isE2E:', isE2E)
 export const auth = betterAuth({
+	advanced: {
+		trustedProxyHeaders: true,
+	},
+	baseURL: {
+		allowedHosts: ['https://poke-deck-xi.vercel.app', '*.vercel.app'],
+	},
 	database: prismaAdapter(prisma, {
 		provider: 'postgresql',
 	}),
@@ -25,31 +31,4 @@ export const auth = betterAuth({
 				]),
 		dash(),
 	],
-	trustedOrigins: request => {
-		const origin = request!.headers.get('origin')
-		if (!origin) return []
-
-		if (process.env.BETTER_AUTH_URL && origin === process.env.BETTER_AUTH_URL) {
-			return [origin]
-		}
-
-		const isPreviewOrE2E =
-			process.env.VERCEL_ENV === 'preview' ||
-			process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview'
-
-		if (isPreviewOrE2E) {
-			const isAllowedProjectDomain =
-				/^https:\/\/poke-deck-[a-zA-Z0-9]{9}-forckes-projects\.vercel\.app$/.test(
-					origin,
-				) ||
-				/^https:\/\/poke-deck-[a-zA-Z0-9-]+\.vercel\.app$/.test(origin) ||
-				origin.startsWith('http://localhost:')
-
-			if (isAllowedProjectDomain) {
-				return [origin]
-			}
-		}
-
-		return []
-	},
 })
